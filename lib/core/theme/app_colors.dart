0d619703d31dfd46e6319ae15f4def2da7287306
rhanifrizal <rhanif.rizal@gmail.com>
2025-09-05 19:34:00 +0800

    app color

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme Colors
+  static const Color lightPrimary = Color(0xFF2196F3);
+  static const Color lightSecondary = Color(0xFF03DAC6);
+  static const Color lightBackground = Color(0xFFFAFAFA);
+  static const Color lightSurface = Color(0xFFFFFFFF);
+  static const Color lightOnPrimary = Color(0xFFFFFFFF);
+  static const Color lightOnSurface = Color(0xFF000000);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color(0xFF2196F3);
+  static const Color darkSecondary = Color(0xFF03DAC6);
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color darkOnPrimary = Color(0xFF000000);
+  static const Color darkOnSurface = Color(0xFFFFFFFF);
+
+  // Status Colors
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFF9800);
+  static const Color error = Color(0xFFF44336);
+  static const Color info = Color(0xFF2196F3);
+
+  // Priority Colors
+  static const Color priorityHigh = Color(0xFFF44336);
+  static const Color priorityMedium = Color(0xFFFF9800);
+  static const Color priorityLow = Color(0xFF4CAF50);
+}
