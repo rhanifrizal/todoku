80ef6e05cc81a32c0090233d20b379f6e54fa3db
rhanifrizal <rhanif.rizal@gmail.com>
2025-09-05 19:34:11 +0800

    app constant

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,18 @@
+
+class AppConstants {
+  static const String appName = "TodoKu";
+  static const String appVersion = "1.0.0";
+
+  // Storage Keys
+  static const String userTokenKey = "user_token";
+  static const String userDataKey = "user_data";
+  static const String themeKey = "theme_mode";
+  static const String languageKey = "language_code";
+
+  // Timeouts
+  static const int connectionTimeout = 30000;
+  static const int receiveTimeout = 30000;
+
+  // Pagination
+  static const int defaultPageSize = 20;
+}
